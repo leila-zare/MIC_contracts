c419049439ebd7d62847077b65edb000de3aebfa
leila zare <leila.zare8996@gmail.com>
2024-12-17 11:35:33 +0330

    v1.3

--- contracts/models.py
+++ contracts/models.py
@@ -79,7 +79,7 @@ class Contract(models.Model):
         # ('Basic', 'پایه'),
         # ('Real', 'واقعی'),
     ], verbose_name="حقوق ابرازی")
-    base_salary = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="حقوق پایه")
+    base_salary = models.DecimalField(max_digits=20, decimal_places=2, validators=[MinValueValidator(1000),MaxValueValidator(10000000)], verbose_name="حقوق پایه")
     housing_allowance = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="حق مسکن")
     food_allowance = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="حق خوار و بار")
     seniority_pay = models.DecimalField(max_digits=20, decimal_places=2, verbose_name="پایه سنوات")
