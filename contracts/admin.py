aa1df7a008755348c47ffff4966381f7eed58687
leila-zare <leila.zare8996@gmail.com>
2024-12-24 10:33:27 +0330

    v1.6

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from . import models
-from jalali_date import datetime2jalali, date2jalali
-from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin, TabularInlineJalaliMixin	
 
 
 # Register your models here.
