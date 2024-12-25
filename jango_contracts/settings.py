f93200f1a3d7935448cedc498cd61f54e1160d76
leila-zare <leila.zare8996@gmail.com>
2024-12-25 17:06:41 +0330

    v2.1

--- jango_contracts/settings.py
+++ jango_contracts/settings.py
@@ -28,12 +28,12 @@ SECRET_KEY = 'django-insecure-9@jqj(604qoobvg801^867qqgqkrzyn18kbvd21$o4bg-opmil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = [] اینجا قبلا برای زمانی بود که نیاز نبود به آی پی خاصی وصل بشم.
-ALLOWED_HOSTS = [
-    '192.168.140.11',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = [] #اینجا قبلا برای زمانی بود که نیاز نبود به آی پی خاصی وصل بشم.
+# ALLOWED_HOSTS = [
+#     '192.168.140.11',
+#     'localhost',
+#     '127.0.0.1',
+# ]
 
 
 # Application definition
@@ -86,16 +86,24 @@ WSGI_APPLICATION = 'jango_contracts.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Kknbuxtr-1371',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
+# DATABASES = {
+#     'default': {
+        
+#         'ENGINE': 'django.db.backends.postgresql',
+        
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Kknbuxtr-1371',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
